8f8281e6c9b5b56db205cd079dbf31f9643629a8
Craig Walls <cwalls@pivotal.io>
2016-12-20 20:26:15 -0700

    Fix controller to call circuit-breaker method.

--- multi-ticket-picker/src/main/java/lotto/MultiPickController.java
+++ multi-ticket-picker/src/main/java/lotto/MultiPickController.java
@@ -1,33 +1,24 @@
 package lotto;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class MultiPickController {
 
-	private MultiPickerProps props;
-	private RestTemplate rest;
+	private MultiPicker multiPicker;
 
 	@Autowired
-	public MultiPickController(MultiPickerProps props, @LoadBalanced RestTemplate rest) {
-		this.props = props;
-		this.rest = rest;
+	public MultiPickController(MultiPicker multiPicker) {
+		this.multiPicker = multiPicker;
 	}
 	
 	@GetMapping("/pick")
 	public List<int[]> pick() {
-		List<int[]> picks = new ArrayList<>();;
-		for(int i = 0; i < props.getTicketCount(); i++) {
-			picks.add(rest.getForObject("http://lotto-picker/random", int[].class));
-		}
-		return picks;
+		return multiPicker.pick();
 	}
 	
 }
--- multi-ticket-picker/src/main/java/lotto/MultiPickerImpl.java
+++ multi-ticket-picker/src/main/java/lotto/MultiPickerImpl.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
+@Service
 public class MultiPickerImpl implements MultiPicker {
 
 	private MultiPickerProps props;
@@ -23,7 +25,7 @@ public class MultiPickerImpl implements MultiPicker {
 	@Override
 	@HystrixCommand(fallbackMethod="defaultPicks")
 	public List<int[]> pick() {
-		List<int[]> picks = new ArrayList<>();;
+		List<int[]> picks = new ArrayList<>();
 		for(int i = 0; i < props.getTicketCount(); i++) {
 			picks.add(rest.getForObject("http://lotto-picker/random", int[].class));
 		}
